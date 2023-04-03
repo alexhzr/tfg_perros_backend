68d9618ee284a0e8ae4acfb866a240885cae9b2c
Alejandro Hernández <alex.hdz94@gmail.com>
2023-04-03 14:11:06 +0200

    Started backend

---
+++ src/test/kotlin/com/alexhernandez/tfgperros/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alexhernandez.tfgperros
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
