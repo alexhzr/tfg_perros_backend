9a49807ae674e5b06dcaac0fe74770ab3d33194d
Álex Hernández <alex.hdz94@gmail.com>
2023-04-06 15:57:51 +0200

    Created new data class
    New data class ServiceResponse to pass operation results from service layer to controller.

---
+++ src/main/kotlin/com/alexhernandez/tfgperros/util/Messages.kt
@@ -0,0 +1,7 @@
+package com.alexhernandez.tfgperros.util
+
+object Messages {
+    const val USER_EXISTS = "User email or DNI already exists"
+    const val USER_REGISTERED = "User successfully registered"
+    const val USER_ERROR_REGISTERING = "There was an error registering"
+}
